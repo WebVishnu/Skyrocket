567fc0a806e65ef1aac2ffa682af8e78c91e5f0e
Vishnu Goswami <87561982+Vishnu9412@users.noreply.github.com>
2023-08-17 22:46:40 +0530

    versal analitics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} overflow-x-hidden`}
       >
+        <Analytics />
         <Navbar />
         {children}
       </body>
